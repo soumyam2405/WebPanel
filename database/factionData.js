f93fc3ab4007baeae48d3bfc4a933f46bf1b32e6
CoolBoy <59187719+soumyam2405@users.noreply.github.com>
2022-09-25 01:05:37 +0530

    mongo stuff

--- database/factionData.js
+++ database/factionData.js
@@ -1,1 +1,30 @@
-require('dotenv').config();
+const mongoose = require('mongoose');
+
+const factionSchema = new mongoose.Schema({
+    name: String,
+    tag: String,
+    id: Number,
+    createdAt: Date,
+});
+
+const factionModel = mongoose.model('factionModel', factionSchema, 'factionData');
+
+async function getFactions() {
+    return await factionModel.find({});
+}
+
+async function createFaction(fac_id, fac_name, fac_tag, fac_createdAt) {
+    await factionModel.replaceOne({ fac_id }, {
+        fac_id,
+        fac_name,
+        fac_tag,
+        fac_createdAt
+    }, {
+        upsert: true
+    });
+}
+
+module.exports = {
+    getFactions,
+    createFaction
+}
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express"),
   app = express();
 require('dotenv').config();
-//setting view engine to ejs
+const mongoose = require('mongoose');
+const { getFactions } = require("./database/factionData");
+
+const main = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.info('Mongo Connected');
+}
+
 app.set("view engine", "ejs");
 
-//route for index page
 app.get("/", function (req, res) {
   const pageTitle = 'Home';
   res.render("index", {
@@ -12,7 +18,6 @@ app.get("/", function (req, res) {
   });
 });
 
-//route for magic page
 app.get("/magic", function (req, res) {
   const pageTitle = 'Magic';
   res.render("magic", {
@@ -20,12 +25,16 @@ app.get("/magic", function (req, res) {
   });
 });
 
-app.get("/faction", function (req, res) {
+app.get("/faction", async function (req, res) {
   const pageTitle = 'Faction';
+  const factionData = await getFactions();
   res.render("faction", {
-    pageTitle: pageTitle
+    pageTitle: pageTitle,
+    factions: factionData
   });
 });
+
 app.listen(8080, function () {
   console.log("Server is running on port 8080 ");
+  main().catch(err => console.warn(err));
 });
